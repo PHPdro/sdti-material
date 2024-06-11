57547733491a979b309c136fbc8742a3a2c94a34
PHPdro <pedro110302@gmail.com>
2024-06-11 18:35:07 -0300

    feat: create transfers filament resource

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -27,6 +27,9 @@ class AdminPanelProvider extends PanelProvider
             ->id('admin')
             ->path('admin')
             ->login()
+            ->registration()
+            ->passwordReset()
+            ->profile(isSimple: false)
             ->colors([
                 'primary' => Color::Amber,
             ])
